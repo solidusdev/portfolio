d0106db0f18242eb43ae0bab1ab31e86b5043044
Ivan <i_cuenca@aol.com>
2020-08-03 17:02:58 -0400

    Updates

---
+++ precache-manifest.bfe04dc41620e702e1e783e185f29bbe.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7870cf9763b91d712e23eb8da4e00769",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae195917c5e24eab8932",
+    "url": "/portfolio/static/css/2.7411d571.chunk.css"
+  },
+  {
+    "revision": "55bfe55c056c38d7e508",
+    "url": "/portfolio/static/css/main.ae6fe6cb.chunk.css"
+  },
+  {
+    "revision": "ae195917c5e24eab8932",
+    "url": "/portfolio/static/js/2.aada8cc6.chunk.js"
+  },
+  {
+    "revision": "5557b1f1d20190131f9fd21a73d4b9e7",
+    "url": "/portfolio/static/js/2.aada8cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55bfe55c056c38d7e508",
+    "url": "/portfolio/static/js/main.2343fd8a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/portfolio/static/media/ProximaNova-Bold.62d4d7d3.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/portfolio/static/media/ProximaNova-Reg.410504d4.otf"
+  },
+  {
+    "revision": "6a386899746222073dd64c5f74d1a69d",
+    "url": "/portfolio/static/media/ProximaNova-Sbold.6a386899.otf"
+  },
+  {
+    "revision": "935d160d64b1d830aaffd1a54ef15b33",
+    "url": "/portfolio/static/media/background.935d160d.png"
+  },
+  {
+    "revision": "68874bf007219b2584200843c133d450",
+    "url": "/portfolio/static/media/colotek.68874bf0.png"
+  },
+  {
+    "revision": "fa8c3bb5956592d8368afa3045920cf9",
+    "url": "/portfolio/static/media/cuny.fa8c3bb5.png"
+  },
+  {
+    "revision": "1b210d1620c4dd22c8cbe411c86184d0",
+    "url": "/portfolio/static/media/google.1b210d16.png"
+  },
+  {
+    "revision": "a08f59f44e32ab5f5fe579b7b4088b97",
+    "url": "/portfolio/static/media/linkedin.a08f59f4.png"
+  },
+  {
+    "revision": "0d21437b44fefda517decd8e16a17341",
+    "url": "/portfolio/static/media/newhouse.0d21437b.png"
+  },
+  {
+    "revision": "62497b8eed7a5ad135d55269c9417ee2",
+    "url": "/portfolio/static/media/project1.62497b8e.png"
+  },
+  {
+    "revision": "000acf3f0e631f66c469a239f1ca3445",
+    "url": "/portfolio/static/media/project2.000acf3f.png"
+  },
+  {
+    "revision": "060869e813890c5e2f832a30aeacc3f9",
+    "url": "/portfolio/static/media/project3.060869e8.png"
+  },
+  {
+    "revision": "9dea590da15d98a1a966086e12c40780",
+    "url": "/portfolio/static/media/project4.9dea590d.png"
+  },
+  {
+    "revision": "56896d4fcf388add90a935297c482bf1",
+    "url": "/portfolio/static/media/project5.56896d4f.png"
+  },
+  {
+    "revision": "3bc004d24363affd403170bd6543894f",
+    "url": "/portfolio/static/media/project6.3bc004d2.png"
+  },
+  {
+    "revision": "d08658b579af913349ca4f674deab146",
+    "url": "/portfolio/static/media/project7.d08658b5.png"
+  },
+  {
+    "revision": "1c685fed8e0e2a576dc0e7cefe1c8401",
+    "url": "/portfolio/static/media/project8.1c685fed.png"
+  }
+]);
